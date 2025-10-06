fe86e56ecde3db2a11f3d70621f426907fe210d4
AlexVennegas <erick.alexis.venegas@gmail.com>
2025-10-06 12:26:54 +0200

    Add HTML tables for school schedules and presentations
    - Created multiple HTML files for school schedules including daily classes and breaks.
    - Added a periodic table representation in HTML format.
    - Developed a detailed schedule for February 2021 with proper headers and captions.
    - Implemented a presentation for Google Chromecast with images and videos.
    - Introduced navigation links for national and international information pages.
    - Added Java classes for bicycle objects and a main class to demonstrate object creation.
    - Updated .gitignore files to exclude unnecessary compiled files and directories.

---
+++ Programacion + Amplicacion/Programs/src/Objetos1/Bicycle.java
@@ -0,0 +1,16 @@
+package Objetos1;
+
+public class Bicycle {
+	
+	
+	public int cadence;
+	public int gear;
+	public int speed;
+	
+	
+	public Bicycle(int startCadence, int startSpeed, int startGear) {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }
+}
---
+++ Programacion + Amplicacion/Programs/src/Objetos1/Principal.java
@@ -0,0 +1,14 @@
+package Objetos1;
+
+public class Principal {
+	public static void main(String[] args) {
+		
+		
+		Bicycle b1;
+		
+		b1 = new Bicycle(1, 2, 3);
+
+		
+		System.out.println(b1.getClass());
+	}
+}
